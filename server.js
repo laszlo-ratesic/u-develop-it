18dc32d447227bb008e267e7afdde1ef296425c0
laszlo-ratesic <keenanrc@hotmail.com>
2022-03-07 10:44:09 -0600

    add CRUD routes to express server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
-const mysql = require('mysql2');
-const gradient = require('gradient-string');
-const express = require('express');
+require("dotenv").config();
+const inputCheck = require('./utils/inputCheck');
+const mysql = require("mysql2");
+const gradient = require("gradient-string");
+const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,55 +12,106 @@ app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        // MySQL username
-        user: process.env.DB_USER,
-        // MySQL password
-        password: process.env.DB_PASSWORD,
-        database: 'election'
-    },
-    console.log(gradient('red', 'white', 'blue')('Connected to the election database.'))
-)
+  {
+    host: "localhost",
+    // MySQL username
+    user: process.env.DB_USER,
+    // MySQL password
+    password: process.env.DB_PASSWORD,
+    database: "election",
+  },
+  console.log(
+    gradient("red", "white", "blue")("Connected to the election database.")
+  )
+);
 
 // GET a list of all potential candidates
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// })
+app.get("/api/candidates", (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
 
 // GET a single candidate
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// })
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
 
 // DELETE a candidate
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
 
 // CREATE a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//                 VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
 
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// })
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
 
 // CATCHALL Default response for any other request (not found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 app.listen(PORT, () => {
-    console.log(gradient.atlas(`Server running on port ${PORT} || http://localhost:${PORT}`));
-})
+  console.log(
+    gradient.atlas(`Server running on port ${PORT} || http://localhost:${PORT}`)
+  );
+});
