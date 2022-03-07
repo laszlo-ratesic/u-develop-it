9493b0b392a12705aff6575e62a8a947b08cd297
laszlo-ratesic <keenanrc@hotmail.com>
2022-03-07 08:50:41 -0600

    add database connection using env variables

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+const gradient = require('gradient-string');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const gradient = require('gradient-string');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Default response for any other request (not found)
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username
+        user: process.env.DB_USER,
+        // MySQL password
+        password: process.env.DB_PASSWORD,
+        database: 'election'
+    },
+    console.log(gradient('red', 'white', 'blue')('Connected to the election database.'))
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
+
+// CATCHALL Default response for any other request (not found)
 app.use((req, res) => {
     res.status(404).end();
 });
